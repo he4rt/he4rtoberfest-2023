48edf47f70d5a33271b6468080adfcfd62133183
Hewerton <hewertonlourenco@gmail.com>
2023-10-03 01:08:16 -0300

    Finalizando o desafio

---
+++ challenges/1-cubo-simples/go/sherlockzen/cubo-simples.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func square(x int) int {
+	return x * x
+}
+
+func main() {
+	var value int
+	fmt.Print("Digite um valor inteiro: ")
+	_, err := fmt.Scanf("%d", &value)
+	if err != nil {
+		fmt.Println("Erro: ", err)
+	}
+	result := square(value)
+	fmt.Println(result)
+}
