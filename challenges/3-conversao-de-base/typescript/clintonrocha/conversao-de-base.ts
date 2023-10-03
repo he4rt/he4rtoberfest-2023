c0f06b3afb0a7802580e3f3b307fcb284668677a
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2023-10-03 19:40:16 -0300

    Finalizando o desafio

--- challenges/3-conversao-de-base/typescript/clintonrocha/conversao-de-base.ts
+++ challenges/3-conversao-de-base/typescript/clintonrocha/conversao-de-base.ts
@@ -4,7 +4,6 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 rl.question("number: ", (input) => {
   console.log((Number(input) >>> 0).toString(2));
   rl.close();
