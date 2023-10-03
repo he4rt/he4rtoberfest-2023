4faac865f2926ecde0747585848e3c0fcb3283b6
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-10-03 09:57:59 -0300

    Finalizando o desafio 1

---
+++ challenges/1-cubo-simples/c/PedroDrago/README.md
@@ -0,0 +1,28 @@
+* Modelo para submissão
+
+Atenção! Siga o modelo abaixo em *markdown* para realizar o envio do =README.md= de cada um de seus exercícios.
+
+#+BEGIN_SRC markdown
+# Submissão de Exercicio
+
+**Exercicio:** 1 - Cubo Simples
+
+**Nickname:** PedroDrago
+
+**Nível Técnico:** - Estudante
+
+**Empresa:** - 42 Rio
+
+**Twitter**: https://twitter.com/PedroDrago
+
+**Dificuldade de Resolução:** - Média
+
+**Comentários:** Por seguir o método da minha escola (42 Rio) foi mais trabalhoso do que seria se tivesse usado apenas um printf simples, mas quis exercitar meus conhecimentos adquiridos recentemente. Tive que fazer diversas funções auxiliares pra poder convertero ARGV para numero, calcular o cubo, e posteriormente converter o cubo de volta para char para poder printar ao final.
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo (OBS: Podem ser passados N argumentos para calculo de cubos): 
+```bash
+cc main.c && ./a.out argv1 argv2 argv3
+```
+#+END_SRC
---
+++ challenges/1-cubo-simples/c/PedroDrago/main.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+
+static void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n == -2147483648)
+		write(1, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			write(1, "-", 1);
+			ft_putnbr(-n);
+		}
+		else if (n > 9)
+		{
+			ft_putnbr(n / 10);
+			ft_putnbr(n % 10);
+		}
+		else
+		{
+			c = n + '0';
+			write(1, &c, 1);
+		}
+	}
+}
+
+static int	ft_atoi(char *n_str)
+{
+	int	number;
+	int	sign;
+	int	count;
+
+	number = 0;
+	sign = 1;
+	count = 0;
+	while ((n_str[count] >= 9 && n_str[count] <= 13) || (n_str[count] == 32))
+		count++;
+	if (n_str[count] == '-')
+		sign = -1;
+	if (n_str[count] == '-' || n_str[count] == '+')
+		count++;
+	while (n_str[count] >= '0' && n_str[count] <= '9')
+		number = number * 10 + (n_str[count++] - '0');
+	return (number * sign);
+}
+
+static int	ft_power(int n, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	else
+		return (n * ft_power(n, power - 1));
+}
+
+int	main(int argc, char *argv[])
+{
+	int	current_cube;
+	int	count;
+
+	(void) argc;
+	count = 0;
+	while (argv[++count])
+	{
+		current_cube = ft_power(ft_atoi(argv[count]), 3);
+		ft_putnbr(current_cube);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
