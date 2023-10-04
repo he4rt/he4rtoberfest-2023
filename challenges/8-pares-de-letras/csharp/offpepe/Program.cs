7495c74796e61de706fec55f886e0ce67eb93a68
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2023-10-04 01:59:31 -0300

    fix: remove stopwatch

--- challenges/8-pares-de-letras/csharp/offpepe/Program.cs
+++ challenges/8-pares-de-letras/csharp/offpepe/Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Diagnostics;
-using System.Threading.Channels;
-
-var input = Console.ReadLine() ?? string.Empty;
-var watch = new Stopwatch();
-watch.Start();
+﻿var input = Console.ReadLine() ?? string.Empty;
 var halfSize = (int) Math.Ceiling(input.Length / 2m);
 var pairVector = new List<string>(halfSize);
 for (var i = 0; i < input.Length; i+= 2)
@@ -16,5 +11,3 @@ for (var i = 0; i < input.Length; i+= 2)
     pairVector.Add($"{input[i]}{input[i+1]}");
 }
 Console.WriteLine($"[{string.Join(", ",pairVector)}]");
-watch.Stop();
-Console.WriteLine(watch.Elapsed);
