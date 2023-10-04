38ec517c8f279d75fbd06727d4f4b959ef44d96f
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-10-03 22:17:27 -0300

    Finalizando o desafio

---
+++ challenges/1-cubo-simples/go/andreyvdl/README.md
@@ -0,0 +1,31 @@
+# Submissão de Exercicio
+
+**Exercicio:** 1 - Cubo Simples
+
+**Nickname:** andreyvdl
+
+**Nível Técnico:** - Estudante
+
+**Empresa:** Nenhuma
+
+**Twitter**: (opcional)
+
+**Dificuldade de Resolução:** Baixa
+
+**Comentários:** Foi legal, fazia tempo que eu não tratava overflow
+
+**Como rodar o desafio**: 
+
+1. tenha Go instalado na sua máquina
+
+2. Use o comando abaixo:
+
+```bash
+go run power3.go
+```
+
+ou
+
+```bash
+go run power3.go number
+```
---
+++ challenges/1-cubo-simples/go/andreyvdl/power3.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func	main() {
+	var	scannedNumber	int64
+
+	if len(os.Args) < 2 {
+		fmt.Println("No argument provided")
+		fmt.Print("Please provide your number: ")
+		_, err := fmt.Scan(&scannedNumber)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+		powerOf3(scannedNumber)
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many args")
+		os.Exit(1)
+	}
+	number, err := strconv.ParseInt(os.Args[1], 10, 64)
+	if (err != nil) {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	powerOf3(number)
+}
+
+func	powerOf3(number int64) {
+	var	overflow	int64
+
+	overflow = number * number * number
+	if overflow / number / number != number {
+		fmt.Println("Error: your number generated a overflow, try a lower number")
+		os.Exit(1)
+	}
+	fmt.Println(overflow)
+}
