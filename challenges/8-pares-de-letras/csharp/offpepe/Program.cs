90640c2f5d0131a646f82bf02b3bf4a078bed0fe
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2023-10-04 01:51:01 -0300

    chore: remove useless char array instantiation

--- challenges/8-pares-de-letras/csharp/offpepe/Program.cs
+++ challenges/8-pares-de-letras/csharp/offpepe/Program.cs
@@ -1,14 +1,20 @@
-﻿var input = Console.ReadLine() ?? string.Empty;
+﻿using System.Diagnostics;
+using System.Threading.Channels;
+
+var input = Console.ReadLine() ?? string.Empty;
+var watch = new Stopwatch();
+watch.Start();
 var halfSize = (int) Math.Ceiling(input.Length / 2m);
 var pairVector = new List<string>(halfSize);
-var chars = input.ToCharArray();
-for (var i = 0; i < chars.Length; i+= 2)
+for (var i = 0; i < input.Length; i+= 2)
 {
-    if (i + 1 >= chars.Length)
+    if (i + 1 >= input.Length)
     {
-        pairVector.Add(chars[^1] + "$");
+        pairVector.Add(input[^1] + "$");
         break;
     } 
-    pairVector.Add($"{chars[i]}{chars[i+1]}");
+    pairVector.Add($"{input[i]}{input[i+1]}");
 }
 Console.WriteLine($"[{string.Join(", ",pairVector)}]");
+watch.Stop();
+Console.WriteLine(watch.Elapsed);
