f9d02b7a2b7247e9cf03c2d04afd4e0eb67f7121
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2023-10-04 01:45:35 -0300

    feat: letter pairs

---
+++ challenges/8-pares-de-letras/csharp/offpepe/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = Console.ReadLine() ?? string.Empty;
+var halfSize = (int) Math.Ceiling(input.Length / 2m);
+var pairVector = new List<string>(halfSize);
+var chars = input.ToCharArray();
+for (var i = 0; i < chars.Length; i+= 2)
+{
+    if (i + 1 >= chars.Length)
+    {
+        pairVector.Add(chars[^1] + "$");
+        break;
+    } 
+    pairVector.Add($"{chars[i]}{chars[i+1]}");
+}
+Console.WriteLine($"[{string.Join(", ",pairVector)}]");
