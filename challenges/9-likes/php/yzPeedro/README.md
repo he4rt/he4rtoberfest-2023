5823fe59e7c948c5055b5c423c72d8d210f3952b
yzPeedro <pedrocruzpessoa16@gmail.com>
2023-10-05 13:28:32 -0300

    Finalizando o desafio

---
+++ challenges/9-likes/php/yzPeedro/README.md
@@ -0,0 +1,27 @@
+# Submissão de Exercicio
+
+**Exercicio:** 9 - Likes
+
+**Nickname:** yzPeedro
+
+**Nível Técnico:** Pleno
+
+**Empresa:** Tecnol Sistema de Informação
+
+**Twitter**: https://twitter.com/_Peedrokas
+
+**Dificuldade de Resolução:** Baixa
+
+**Comentários:**
+
+**Como rodar o desafio**:
+
+Dentro do diretório do projeto, utilize o comando abaixo:
+
+```bash
+php index.php []
+php index.php ['Daniel']
+php index.php ['Daniel','Guto']
+php index.php ['Daniel','Guto','Cherry']
+php index.php ['Daniel','Guto','Cherry','Marlon']
+```
---
+++ challenges/9-likes/php/yzPeedro/index.php
@@ -0,0 +1,20 @@
+<?php
+
+if (! isset($argv[1])) {
+    die("Por favor informe o parâmetro de entrada");
+}
+
+$string = str_replace(['[', ']'], '', $argv[1]);
+$likes = array_filter(
+    array_map(fn(string $name) => str_replace("'", '', trim($name)),
+    explode(',', $string))
+);
+
+
+echo match (count($likes)) {
+    0 => "Ninguém curtiu isso",
+    1 => "{$likes[0]} curtiu isso",
+    2 => "{$likes[0]} e {$likes[1]} curtiram isso",
+    3 => "{$likes[0]}, {$likes[1]} e {$likes[2]} curtiram isso",
+    default => "{$likes[0]}, {$likes[1]} e " . (count($likes) - 2) . " pessoas curtiram isso",
+};
