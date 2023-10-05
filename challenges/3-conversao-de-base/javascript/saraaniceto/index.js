ce0725b8ac5b8eb6897db80600840635195b88e7
Sara Aniceto <sara.aniceto@gmail.com>
2023-10-05 09:46:46 +0200

    Desafio 3 refeito

--- challenges/3-conversao-de-base/javascript/saraaniceto/index.js
+++ challenges/3-conversao-de-base/javascript/saraaniceto/index.js
@@ -1,25 +1,8 @@
-const baseDec = process.argv.slice(2)
+const baseDec = process.argv.slice(2);
 
-for (var i = 0; i < baseDec.length; i++){
-    let num = Number(baseDec[i])
-    let div = Math.floor(num / 2)
-    let restos = []
-    let rest = num % 2
-    restos.push(rest)
+for (var i = 0; i < baseDec.length; i++) {
+  let num = +baseDec[i];
+  let binario = num.toString(2);
+  console.log(binario);
 
-    while(div > 2) {
-        div = Math.floor(div / 2)
-        rest = div % 2
-        restos.push(rest)     
-        var bin0 = div        
-    }
-
-    if (div == 2){
-        bin0 = Math.floor(div/2)
-        rest = div % 2
-        restos.push(rest)
-    }
-
-    let arrayBin = [bin0, restos.join('')]
-    console.log(`${num} = ${arrayBin.join('')}`)
 }
