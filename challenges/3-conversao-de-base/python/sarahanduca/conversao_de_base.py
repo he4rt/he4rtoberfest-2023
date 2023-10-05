1857d238e065c136a71e532d3821bfa21ad794ad
sarahanduca <anduca.sarah@gmail.com>
2023-10-05 19:50:41 -0300

    Desafio finalizado

--- challenges/3-conversao-de-base/python/sarahanduca/conversao_de_base.py
+++ challenges/3-conversao-de-base/python/sarahanduca/conversao_de_base.py
@@ -1,14 +1,12 @@
-import math 
+import sys
 def to_binary(decimal):
     binary = []
-    binary_range = math.ceil(math.sqrt(decimal))
-    for digit in range(binary_range):
-        binary.append(decimal%2)
-
+    while(decimal > 1):
+        binary.append(decimal % 2)
+        decimal = decimal // 2
+    binary.append(decimal)
+    binary.reverse()
     return " ".join(str(e) for e in binary)
 
-print(to_binary(27))
-print(to_binary(64))
-print(to_binary(16))
-print(to_binary(2))
-print(to_binary(7))
+num = int(sys.argv[1])
+print(to_binary(num))
