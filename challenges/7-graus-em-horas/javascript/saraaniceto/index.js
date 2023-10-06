2987e59953eb26d16e3beb6755a1bb0b3fb6cd61
Sara Aniceto <sara.aniceto@gmail.com>
2023-10-07 00:44:25 +0200

    Melhorando o código

--- challenges/7-graus-em-horas/javascript/saraaniceto/index.js
+++ challenges/7-graus-em-horas/javascript/saraaniceto/index.js
@@ -2,8 +2,9 @@ const graus = process.argv[2]
 let grauHora = 180 / 6
 let grauMinuto = grauHora / 60
  
-let horas = parseInt(graus / grauHora)
-let minutos = parseInt((graus % grauHora) / grauMinuto)
+let minutos = graus / grauMinuto
+let horas = parseInt( minutos / 60 )
+minutos = minutos % 60
 
 if (minutos < 10){
     console.log(`${horas}:0${minutos}`)
