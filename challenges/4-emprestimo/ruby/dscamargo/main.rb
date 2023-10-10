ea4527b76b8624388d32a0cbcc3a348c6d9fe475
Douglas Simon Camargo <douglas_simon@hotmail.com>
2023-10-10 13:46:32 -0300

    Ajuste ao capturar argumento

--- challenges/4-emprestimo/ruby/dscamargo/main.rb
+++ challenges/4-emprestimo/ruby/dscamargo/main.rb
@@ -1,5 +1,7 @@
-valor_emprestimo = ARGV[0].to_f
-taxa_juros = ARGV[1].to_f / 100
-tempo_anos = ARGV[2].to_i
+args = ARGV[0].split(",")
+
+valor_emprestimo = args[0].to_f
+taxa_juros = args[1].to_f / 100
+tempo_anos = args[2].to_i
 
 puts (valor_emprestimo * (1 + taxa_juros) ** tempo_anos).round 2
