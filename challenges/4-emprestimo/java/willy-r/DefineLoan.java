ac3e04074fa1244045b031085fbce3c6b1e27605
William Rodrigues <williamrodrigues2442@gmail.com>
2023-10-09 22:21:14 -0300

    hotrefactor: uses bigdecimal for monetary value and parses values as integer

--- challenges/4-emprestimo/java/willy-r/DefineLoan.java
+++ challenges/4-emprestimo/java/willy-r/DefineLoan.java
@@ -1,12 +1,13 @@
+import java.math.BigDecimal;
 import java.util.Locale;
 
 public class DefineLoan {
     public static void main(String[] args) {
         String[] input = args[0].replaceAll(" ", "").split(",");
-        double loanValue = Double.parseDouble(input[0]),
-            interestRate = Double.parseDouble(input[1]),
-            years = Double.parseDouble(input[2]);
-        double output = loanValue * Math.pow(1 + interestRate / 100, years);
+        var loanValue = new BigDecimal(input[0]);
+        double interestRate = (double) Integer.parseInt(input[1]),
+            years = (double) Integer.parseInt(input[2]);
+        var output = loanValue.multiply(new BigDecimal(Math.pow(1 + interestRate / 100, years)));
         Locale.setDefault(Locale.US);
         System.out.printf("%.2f\n", output);
     }
