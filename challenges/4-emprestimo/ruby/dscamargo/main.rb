a4eb27cf5367126390c09a69a31c782df97f4915
Douglas Simon Camargo <douglas_simon@hotmail.com>
2023-10-10 14:08:37 -0300

    fix output format

--- challenges/4-emprestimo/ruby/dscamargo/main.rb
+++ challenges/4-emprestimo/ruby/dscamargo/main.rb
@@ -4,4 +4,4 @@ valor_emprestimo = args[0].to_f
 taxa_juros = args[1].to_f / 100
 tempo_anos = args[2].to_i
 
-puts (valor_emprestimo * (1 + taxa_juros) ** tempo_anos).round 2
+puts "%0.2f" % (valor_emprestimo * (1 + taxa_juros) ** tempo_anos)
