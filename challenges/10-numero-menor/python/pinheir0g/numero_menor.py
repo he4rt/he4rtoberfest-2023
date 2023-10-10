1d5281af8cdffefe95f58b30781ee4191dd1e903
pinheir0g <gv.pinheiro@outlook.com>
2023-10-10 10:21:34 -0300

    Corrigindo o desafio

--- challenges/10-numero-menor/python/pinheir0g/numero_menor.py
+++ challenges/10-numero-menor/python/pinheir0g/numero_menor.py
@@ -1,16 +1,19 @@
 def numero_menor(numero):
-    numeros = [int(d) for d in str(numero)]
-
     if numero > 1:
+        numeros = [int(d) for d in str(numero)]
         reverse = -1
         
         for i in range(len(numeros) - 1):
-            if numeros[i] > numeros[i + 1]:
-                reverse = i
-                break
+                if numeros[i] > numeros[i + 1]:
+                    reverse = i
+                    break
+
         if reverse == -1:
+             return -1
+        
+        if all(numeros[i] == numeros[reverse + 1] for i in range(reverse + 1, len(numeros))):
             return -1
-    
+        
         numeros[reverse + 1:] = reversed(numeros[reverse + 1:])
         numeros[reverse + 1:] = sorted(numeros[reverse + 1:])
         
@@ -18,7 +21,7 @@ def numero_menor(numero):
 
         return numero_novo
     else:
-        return None
+        return -1
     
 
 numero = int(input('Digite um número: '))
